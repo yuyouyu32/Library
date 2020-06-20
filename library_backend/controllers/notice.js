3a2614d564e6edaa370a6f6f4cbb517ce4bec5d3
yyy624297803 <51949655+yyy624297803@users.noreply.github.com>
2020-06-20 13:24:24 +0800

    Update notice.js

--- library_backend/controllers/notice.js
+++ library_backend/controllers/notice.js
@@ -32,11 +32,11 @@ Date.prototype.Format = function(fmt)
 //nodemailer发件人信息
 const mailTransport = nodemailer.createTransport({
     host : 'smtp.qq.com',
-    port: 465,
+    port: ***,
     secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
     auth : {
-        user : '1589972754@qq.com',
-        pass : 'pzbyebkodfnziejf'
+        user : '***********@qq.com',
+        pass : '************'
     },
 });
 
